a97984cd617627e956d203806c549755ab5fed80
odedpriva <odedpriva@gmail.com>
2017-06-18 19:47:38 +0300

    init

---
+++ docker-meetup1/01-naive/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const http = require('http')
+    , os = require('os')
+    , ip = require('ip')
+    , _ = require('lodash');
+
+const PORT=8000;
+
+function handleRequest(request, response){
+
+  let host = os.hostname();
+  
+  let resObj = JSON.stringify({hostName: host, ip:_.split(ip.address(),'.')});
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  response.end(resObj);
+
+}
+ 
+let server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+  console.log('Server listening on: http://localhost:%s', PORT);
+});
